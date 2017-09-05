e94ada4365b37691e33ca3ccd804b50c1a182238
kaixin <22346789@qq.com>
2017-09-05 17:34:37 +0800

    add Test

--- src/com/lijian/Main.java
+++ src/com/lijian/Main.java
@@ -1,14 +1,18 @@
 package com.lijian;
 
+import java.io.EOFException;
 import java.lang.*;
+import java.util.*;
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello world!");
-        Test  t = new Test();
-        t.display();
-
+    public static void main(String[] args)  {
+       int i = Test.id ;
+       Test.setId(2);
+       int after = Test.id;
+       System.out.println("before i = " + i + "\nAfter i = " + after);
     }
+
+
+
 }
 
--- src/com/lijian/Test.java
+++ src/com/lijian/Test.java
@@ -1,7 +1,22 @@
 package com.lijian;
 
+import java.util.*;
+
 public class Test {
-    protected   void display(){
-        System.out.println("call other class");
+    private String name;
+    static int  id;
+    public Test(String name) {
+        this.name = name;
+    }
+    public void setName(String name){
+        String str = "Add";
+        this.name = name + str;
+    }
+    public String getName(){
+        return this.name;
+    }
+    static void setId(int i){
+        // System.out.println(name); 无法从静态上下文中引用非静态 变量 name
+        Test.id = i;
     }
 }
