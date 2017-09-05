8eebc4e0e59e4e89ddd03f96341ca108e41134bf
kaixin <22346789@qq.com>
2017-09-05 15:21:42 +0800

    Initial commit

---
+++ src/com/lijian/Main.java
@@ -0,0 +1,14 @@
+package com.lijian;
+
+import java.lang.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world!");
+        Test  t = new Test();
+        t.display();
+
+    }
+}
+
---
+++ src/com/lijian/Test.java
@@ -0,0 +1,7 @@
+package com.lijian;
+
+public class Test {
+    protected   void display(){
+        System.out.println("call other class");
+    }
+}
