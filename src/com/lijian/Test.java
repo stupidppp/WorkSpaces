6eb9ce1745a831ded1eb4a23a39a7becb885e852
kaixin <22346789@qq.com>
2017-09-07 16:38:58 +0800

    Test Class - > extends   override   overloading

--- src/com/lijian/Test.java
+++ src/com/lijian/Test.java
@@ -1,10 +1,10 @@
 package com.lijian;
 
-import java.util.*;
+// 方法的重写(Overriding)和重载(Overloading)
 
-public class Test {
+public class Test  implements  Comparable<Test> {
     private String name;
-    static int  id;
+    private static int  id;
     public Test(String name) {
         this.name = name;
     }
@@ -18,5 +18,42 @@ public class Test {
     static void setId(int i){
         // System.out.println(name); 无法从静态上下文中引用非静态 变量 name
         Test.id = i;
+    }
+    public int compareTo(Test t){
+        return name.compareTo(t.getName());
+    }
+    protected  void f (){
+        System.out.println("protected method!");
+    }
+}
+
+class T2 extends Test{
+     T2(String n){
+
+         super(n);
+     }
+     public void getProtectedMethod(){
+         super.f();
+     }
+     public void f(){
+         System.out.println("protect -> public");
+     }
+     public void f(int a){
+
+     }
+}
+
+
+
+class  inclass {
+    private static inclass  sl;
+    private inclass(){
+        System.out.println("ok");
+    }
+
+    public  static inclass newInClass(){
+        if (sl == null)
+            sl = new inclass();
+        return sl;
     }
 }
